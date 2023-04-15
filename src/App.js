da75a1b090743e6fbe0212d8478cd49b119eac98
정제우 <wpdn3825@naver.com>
2023-04-15 18:57:18 +0900

    실습 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 import { useState } from "react";
-import { createStore } from "redux";
+import { legacy_createStore as createStore } from "redux";
 import { Provider, useSelector, useDispatch, connect } from "react-redux";
 
 function reducer(currentState, action) {
@@ -14,21 +14,39 @@ function reducer(currentState, action) {
 
   // 리엑트 불변성의 원칙을 고수하여 원본을 복사하여 사용한다.
   const newState = { ...currentState };
+  // Onclick 이벤트로 action.type이 PLUS인 경우 실행
+  if (action.type === "PLUS") {
+    newState.number += 1;
+  }
+  if (action.type === "MINUS") {
+    newState.number -= 1;
+  }
   return newState;
 }
+
 const store = createStore(reducer);
 
 function App() {
   const [number, setNumber] = useState(1);
 
+  const jsx = `Provider store={store}`;
+
   return (
-    <div id="container">
-      <h1>Root </h1>
-      <div className="grid">
-        <Left1 number={number}></Left1>
-        <Right1></Right1>
-      </div>
-    </div>
+    <fieldset>
+      <legend>
+        <h1>{jsx}</h1>
+      </legend>
+
+      <Provider store={store}>
+        <div id="container">
+          <h1>Root </h1>
+          <div className="grid">
+            <Left1 number={number}></Left1>
+            <Right1></Right1>
+          </div>
+        </div>
+      </Provider>
+    </fieldset>
   );
 }
 
@@ -51,9 +69,16 @@ function Left2(props) {
 }
 
 function Left3(props) {
+  // useSelector 는 함수를 인자로 받는다.
+  function funcNumber(state) {
+    return state.number; // number state를 쓰겠다~
+  }
+
+  const number = useSelector((state) => state.number);
+
   return (
     <div>
-      <h1>Left3 :</h1>
+      <h1>Left3 :{number}</h1>
     </div>
   );
 }
@@ -76,10 +101,24 @@ function Right2(props) {
 }
 
 function Right3(props) {
+  const dispatch = useDispatch();
   return (
     <div>
       <h1>Right3</h1>
-      <input type="button" value="+" />
+      <input
+        type="button"
+        value="+"
+        onClick={() => {
+          dispatch({ type: "PLUS" });
+        }}
+      />
+      <input
+        type="button"
+        value="-"
+        onClick={() => {
+          dispatch({ type: "MINUS" });
+        }}
+      />
     </div>
   );
 }
