57f71d29a7b318b8e41124f50f323f667a8b801c
정제우 <wpdn3825@naver.com>
2023-04-15 17:55:05 +0900

    install redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from "react";
 import "./style.css";
 import { useState } from "react";
+import { createStore } from "redux";
+import { Provider, useSelector, useDispatch, connect } from "react-redux";
+
+function reducer(currentState, action) {
+  // 기본 state 설정
+  if (currentState === undefined) {
+    return {
+      number: 1,
+    };
+  }
+
+  // 리엑트 불변성의 원칙을 고수하여 원본을 복사하여 사용한다.
+  const newState = { ...currentState };
+  return newState;
+}
+const store = createStore(reducer);
 
 function App() {
   const [number, setNumber] = useState(1);
+
   return (
     <div id="container">
-      <h1>Root : {number}</h1>
+      <h1>Root </h1>
       <div className="grid">
         <Left1 number={number}></Left1>
-        <Right1
-          onIncrease={() => {
-            setNumber(number + 1);
-          }}
-        ></Right1>
+        <Right1></Right1>
       </div>
     </div>
   );
@@ -22,8 +35,8 @@ function App() {
 function Left1(props) {
   return (
     <div>
-      <h1>Left1 : {props.number}</h1>
-      <Left2 number={props.number}></Left2>
+      <h1>Left1 : </h1>
+      <Left2></Left2>
     </div>
   );
 }
@@ -31,8 +44,8 @@ function Left1(props) {
 function Left2(props) {
   return (
     <div>
-      <h1>Left2 : {props.number}</h1>
-      <Left3 number={props.number}></Left3>
+      <h1>Left2 : </h1>
+      <Left3></Left3>
     </div>
   );
 }
@@ -40,7 +53,7 @@ function Left2(props) {
 function Left3(props) {
   return (
     <div>
-      <h1>Left3 : {props.number}</h1>
+      <h1>Left3 :</h1>
     </div>
   );
 }
@@ -49,11 +62,7 @@ function Right1(props) {
   return (
     <div>
       <h1>Right1</h1>
-      <Right2
-        onIncrease={() => {
-          props.onIncrease();
-        }}
-      ></Right2>
+      <Right2></Right2>
     </div>
   );
 }
@@ -61,11 +70,7 @@ function Right2(props) {
   return (
     <div>
       <h1>Right2</h1>
-      <Right3
-        onIncrease={() => {
-          props.onIncrease();
-        }}
-      ></Right3>
+      <Right3></Right3>
     </div>
   );
 }
@@ -74,13 +79,7 @@ function Right3(props) {
   return (
     <div>
       <h1>Right3</h1>
-      <input
-        type="button"
-        value="+"
-        onClick={() => {
-          props.onIncrease();
-        }}
-      />
+      <input type="button" value="+" />
     </div>
   );
 }
