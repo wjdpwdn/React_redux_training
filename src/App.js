78280b7872dde3ef5a9acd2922ab8d10d4c7a961
정제우 <wpdn3825@naver.com>
2023-04-15 17:42:19 +0900

    right 생성

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ function App() {
   const [number, setNumber] = useState(1);
   return (
     <div id="container">
-      <h1>Root</h1>
-      <Left1 number={number}></Left1>
+      <h1>Root : {number}</h1>
+      <div className="grid">
+        <Left1 number={number}></Left1>
+        <Right1
+          onIncrease={() => {
+            setNumber(number + 1);
+          }}
+        ></Right1>
+      </div>
     </div>
   );
 }
@@ -34,6 +41,46 @@ function Left3(props) {
   return (
     <div>
       <h1>Left3 : {props.number}</h1>
+    </div>
+  );
+}
+
+function Right1(props) {
+  return (
+    <div>
+      <h1>Right1</h1>
+      <Right2
+        onIncrease={() => {
+          props.onIncrease();
+        }}
+      ></Right2>
+    </div>
+  );
+}
+function Right2(props) {
+  return (
+    <div>
+      <h1>Right2</h1>
+      <Right3
+        onIncrease={() => {
+          props.onIncrease();
+        }}
+      ></Right3>
+    </div>
+  );
+}
+
+function Right3(props) {
+  return (
+    <div>
+      <h1>Right3</h1>
+      <input
+        type="button"
+        value="+"
+        onClick={() => {
+          props.onIncrease();
+        }}
+      />
     </div>
   );
 }
