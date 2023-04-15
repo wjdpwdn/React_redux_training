05f307edb71f311b5c73802e69de714d8a3647d6
정제우 <wpdn3825@naver.com>
2023-04-15 17:00:18 +0900

    feat : 상황연출 완성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import "./App.css";
+import React from "react";
+import "./style.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div id="container">
+      <h1>Root</h1>
+      <Left1></Left1>
+    </div>
+  );
+}
+
+function Left1(props) {
+  return (
+    <div>
+      <h1>Left1</h1>
+      <Left2></Left2>
+    </div>
+  );
+}
+
+function Left2(props) {
+  return (
+    <div>
+      <h1>Left2</h1>
+      <Left3></Left3>
+    </div>
+  );
+}
+
+function Left3(props) {
+  return (
+    <div>
+      <h1>Left3</h1>
+    </div>
+  );
 }
 
 export default App;
